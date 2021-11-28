1c62210838dedf6ed5e9527973b34eaae72c6c33
thatchedroof <tnert.wire100@gmail.com>
2021-11-27 18:08:14 -0800

    Remove three.js package

---
+++ space/app.d.ts
@@ -0,0 +1,5 @@
+declare var THREE: any;
+declare var camera: any, scene: any, renderer: any;
+declare var geometry: any, material: any, mesh: any;
+declare function init(): void;
+declare function animate(): void;
