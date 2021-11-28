ecf21f3bb0bcd0d380bb08de13ca372b773ae39d
thatchedroof <tnert.wire100@gmail.com>
2021-11-27 20:42:08 -0800

    Revert "Remove three.js package"
    This reverts commit 1c62210838dedf6ed5e9527973b34eaae72c6c33.

--- space/app.d.ts
+++
@@ -1,5 +0,0 @@
-declare var THREE: any;
-declare var camera: any, scene: any, renderer: any;
-declare var geometry: any, material: any, mesh: any;
-declare function init(): void;
-declare function animate(): void;
