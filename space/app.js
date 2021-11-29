4f947154e6fb272518cd9b0b8863d039bd56c9e0
thatchedroof <tnert.wire100@gmail.com>
2021-11-29 03:36:26 -0800

    Add space animation

--- space/app.js
+++ space/app.js
@@ -1,3 +1,70 @@
-'use strict';
-
-var THREE = require("three");
+import * as THREE from './node_modules/three/build/three.module.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+import * as space from './space';
+init();
+function init() {
+    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#canvas'),
+        antialias: true
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.z = 30;
+    camera.rotation.z = Math.PI;
+    const scene = new THREE.Scene();
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const material = new THREE.MeshNormalMaterial();
+    //const mesh = new THREE.Mesh(geometry, material);
+    //scene.add(mesh);
+    const gridHelper = new THREE.GridHelper(200, 50);
+    scene.add(gridHelper);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const background = new THREE.TextureLoader().load('space.jpg');
+    scene.background = background;
+    const equi = new THREE.TextureLoader().load('./benequi.png');
+    const planet = new THREE.Mesh(new THREE.SphereGeometry(.05, 32, 32), new THREE.MeshBasicMaterial({
+        map: equi
+    }));
+    scene.add(planet);
+    const moon = new THREE.Mesh(new THREE.SphereGeometry(.02, 32, 32), new THREE.MeshBasicMaterial({
+        map: equi
+    }));
+    scene.add(moon);
+    const origin = space.orbitCoordinates(space.earthMoonSystem, 0)[0];
+    const setOrigin = [origin[0], origin[1], origin[2] - 50];
+    controls.target.set(...setOrigin);
+    let epoch = 0;
+    function animate() {
+        requestAnimationFrame(animate);
+        //mesh.rotation.x += 0.05;
+        //mesh.rotation.y += 0.02;
+        epoch += .0001;
+        let orbitCoordinates = space.orbitCoordinates(space.earthMoonSystem, epoch);
+        let [x, y, z] = orbitCoordinates[0];
+        let [x2, y2, z2] = orbitCoordinates[1];
+        planet.position.x = x;
+        planet.position.y = y;
+        planet.position.z = z;
+        moon.position.x = x2;
+        moon.position.y = y2;
+        moon.position.z = z2;
+        var dotGeometry = new THREE.BufferGeometry();
+        dotGeometry.setAttribute('position', new THREE.Float32BufferAttribute(new THREE.Vector3(x, y, z).toArray(), 3));
+        var dotMaterial = new THREE.PointsMaterial({ size: 0.1 });
+        var dot = new THREE.Points(dotGeometry, dotMaterial);
+        scene.add(dot);
+        var dotGeometry = new THREE.BufferGeometry();
+        dotGeometry.setAttribute('position', new THREE.Float32BufferAttribute(new THREE.Vector3(x2, y2, z2).toArray(), 3));
+        var dotMaterial = new THREE.PointsMaterial({ size: 0.1 });
+        var dot = new THREE.Points(dotGeometry, dotMaterial);
+        scene.add(dot);
+        camera.position.x = x;
+        camera.position.y = y;
+        camera.position.z = z + 3;
+        //console.log(planet.position.x, planet.position.y, r, theta);
+        controls.update();
+        renderer.render(scene, camera);
+    }
+    animate();
+}
---
+++ space/space.js
@@ -0,0 +1,165 @@
+export function planetDensity(planet) {
+    return planet.mass / (planet.radius * planet.radius * planet.radius);
+}
+export function planetGravity(planet) {
+    return planet.mass / (planet.radius * planet.radius);
+}
+const giant = {
+    mass: 594.4858,
+    radius: 11.444303
+};
+const nine = {
+    mass: 594.4858,
+    radius: 11.444303
+};
+const earth = {
+    mass: 1,
+    radius: 6378.137
+};
+const moon = {
+    mass: 0.0123032,
+    radius: 1738.1
+};
+const a = {
+    mass: 404698.375
+};
+const b = {
+    mass: 263859.877
+};
+const sun = {
+    mass: 333030
+};
+const ab = {
+    starA: a,
+    starB: b,
+    a: 0.2731,
+    e: 0.4178,
+    i: 0,
+    Ω: 0,
+    ω: 0,
+    θ: 0,
+};
+export function mass(input) {
+    if ('mass' in input) {
+        return input.mass;
+    }
+    if ('starA' in input) {
+        return input.starA.mass + input.starB.mass;
+    }
+    if ('planetA' in input) {
+        return input.planetA.mass + input.planetB.mass;
+    }
+}
+export function trueAnomaly(input, epoch) {
+    let mean = meanAnomaly(input, epoch);
+    let iter = mean * (Math.PI / 180);
+    for (var i = 0; i < 100; i++) {
+        iter = mean * (Math.PI / 180) + input.orbit.e * Math.sin(iter);
+    }
+    const out = ((((2 * Math.atan(Math.pow((1 + input.orbit.e) / (1 - input.orbit.e), 1 / 2) * Math.tan(iter / 2))) * (180 / Math.PI)) % 360) + 360) % 360;
+    return out * (Math.PI / 180);
+}
+export function meanAnomaly(input, epoch) {
+    return (epoch / planetaryYear(input)) * 360;
+}
+export function planetaryYear(input) {
+    return Math.sqrt(Math.pow(input.orbit.a, 3) / (mass(input.main)));
+}
+// sqrt(0.00256955529 ** 3 / (1 / x)) = 0.07480519764
+// 0.00256955529 ** 3 / (1 / x) = 0.07480519764 ** 2
+// 0.00256955529 ** 3 * x / 1 = 0.07480519764 ** 2
+//console.log(((0.07480233042 ** 2) * 1) / (0.00256955529 ** 3));
+//console.log(Math.sqrt(0.00256955529 ** 3 / (1 / 333030)));
+export function orbitDistance(input, epoch) {
+    return (input.orbit.a * (1 - input.orbit.e)) / (1 + input.orbit.e * Math.cos(trueAnomaly(input, epoch)));
+}
+export function systemPairs(input) {
+    return input.orbits.map((orbit) => {
+        return {
+            main: input.main,
+            orbit: orbit
+        };
+    });
+}
+export function orbitCoordinates(inp, epoch) {
+    return systemPairs(inp).map((input) => {
+        const r = orbitDistance(input, epoch);
+        const ν = trueAnomaly(input, epoch);
+        const i = input.orbit.i;
+        const Ω = input.orbit.Ω;
+        const ω = input.orbit.ω;
+        //X = r*(np.cos(Om)*np.cos(w+nu) - np.sin(Om)*np.sin(w+nu)*np.cos(i))
+        //Y = r*(np.sin(Om)*np.cos(w+nu) + np.cos(Om)*np.sin(w+nu)*np.cos(i))
+        //Z = r*(np.sin(i)*np.sin(w+nu))
+        const x = r * (Math.cos(Ω) * Math.cos(ω + ν) - Math.sin(Ω) * Math.sin(ω + ν) * Math.cos(i));
+        const y = r * (Math.sin(Ω) * Math.cos(ω + ν) + Math.cos(Ω) * Math.sin(ω + ν) * Math.cos(i));
+        const z = r * (Math.sin(i) * Math.sin(ω + ν));
+        //let x = r * Math.sin(theta) * Math.cos(phi);
+        //let y = r * Math.sin(theta) * Math.sin(phi);
+        //let z = r * Math.cos(theta);
+        const relativeSatellites = orbitCoordinates(input.orbit.body, epoch);
+        const satellites = relativeSatellites.map((sat) => {
+            return [sat[0] + x, sat[1] + y, sat[2] + z];
+        });
+        return [[x, y, z], ...satellites];
+    }).flat();
+}
+export let auScale = 40;
+export const system = {
+    main: ab,
+    orbits: [{
+            body: {
+                main: giant,
+                orbits: [{
+                        body: {
+                            main: nine,
+                            orbits: []
+                        },
+                        a: 0.00826651492403 * auScale,
+                        e: 0.00074,
+                        i: 33.7 * (Math.PI / 180),
+                        Ω: 0,
+                        ω: 0,
+                        θ: 0,
+                    }]
+            },
+            a: 1.1106 * auScale,
+            e: 0.00021,
+            i: 58.23 * (Math.PI / 180),
+            Ω: 0,
+            ω: 0,
+            θ: 0,
+        }]
+};
+export const earthMoonSystem = {
+    main: sun,
+    orbits: [{
+            body: {
+                main: earth,
+                orbits: [{
+                        body: {
+                            main: moon,
+                            orbits: []
+                        },
+                        a: 0.00256955529 * auScale,
+                        e: 0.0549,
+                        i: 5.145 * (Math.PI / 180),
+                        Ω: 0,
+                        ω: 0,
+                        θ: 0,
+                    }]
+            },
+            a: 1 * auScale,
+            e: 0.0167,
+            i: 0,
+            Ω: 0,
+            ω: 0,
+            θ: 0,
+        }]
+};
+for (let i = 0; i < 100; i++) {
+    console.log(systemPairs(system).map((input) => { return trueAnomaly(input, i / 100); })[0]);
+    //console.log(systemPairs(system).map((input) => { return orbitDistance(input, i / 100); })[0]);
+}
+console.log(systemPairs(systemPairs(earthMoonSystem)[0].orbit.body)[0]);
+console.log(planetaryYear(systemPairs(systemPairs(earthMoonSystem)[0].orbit.body)[0]));
